b50cdfc4e2a77c5fa31a00099b8d9eccfe554bd5
radingamer-sketch <radingamer@gmail.com>
2026-08-30 21:00:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-const C='sav-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='sav-v3';
+const ASSETS=['./','./index.html','./manifest.json','./logo.png'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(async c=>{
+    for(const a of ASSETS){try{await c.add(a)}catch(err){}}
+  }));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r;
+  }).catch(()=>caches.match(e.request)));
+});
